60b29f2c6aec780572b1c1154c6c7b5249b95533
Apoorv Saini <apoorvsaini9@gmail.com>
2018-09-11 23:25:23 -0400

    Added promises to fix DB query resolution issues

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 2 API endpoints:
 
-1. /stream/start/<userID> to start stream
-   
-   It will check the current number of streams the user is viewing and give error if user is watching more than 3 streams.
-   
+1. /stream/start/<userID> to start stream: It will check the current number of streams the user is viewing and give error if user is watching more than 3 streams.
+
 2. /stream/stop/<userID>to stop the stream
 
 Uses MongoDB service (Atlas) to track users and streams
--- api/stream.js
+++ api/stream.js
@@ -1,24 +1,29 @@
 const Config = require('../config');
 let MongoClient = require('mongodb').MongoClient;
 
-module.exports.start = function (userId) {
-    MongoClient.connect(Config.MONGO_URI, function(err, db) {
-        if (err) throw err;
-        let dbo = db.db("dazn");
-        let query = { user_id: userId};
-        dbo.collection("users").findOne(query, function(err, result) {
+module.exports.start = async function (userId) {
+    let promise = new Promise((resolve, reject) => {
+        MongoClient.connect(Config.MONGO_URI, { useNewUrlParser: true }, function(err, db) {
             if (err) throw err;
+            let dbo = db.db("dazn");
+            let query = { user_id: userId};
 
-            if (result !== null) {
-                // User found, now check the stream
-                return 'found';
-            }
-
-            db.close();
+            dbo.collection("users").findOne(query, function(err, result) {
+                if (err) throw err;
+                db.close();
+                if (result !== null) {
+                    // User found, now check the stream
+                    resolve('found');
+                }
+                else {
+                    // Create the user
+                    resolve('created');
+                }
+            });
         });
     });
-
-    return 'started';
+    let result = await promise;
+    return result;
 };
 
 module.exports.end = function (userId) {
